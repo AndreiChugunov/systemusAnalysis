8bdf90a0adbe3da0f8e326fea6850728202c454d
Andrei <bloodyavangard@mail.ru>
2017-03-17 03:42:33 +0300

    cool stuff added

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import javax.swing.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         final int N = 2;
+        final int Z = 0;
         List<Work> allWorksList = new ArrayList<>();
         Work dW = new Work("dw", 0, 0, null);
         dW.jobIsDone();
@@ -52,6 +56,7 @@ public class Main {
         List<Work> possibleWorks = new ArrayList<>();
         List<Work> allDoneWorks = new ArrayList<>();
         List<Work> inTimeWorks = new ArrayList<>();
+        List<Work> currentlyDoneWorks = new ArrayList<>();
 
         int time = 0;
         while (allDoneWorks.size() != allWorksList.size()) {
@@ -59,6 +64,7 @@ public class Main {
             for (Work inTimeWork : inTimeWorks) {
                 if (inTimeWork.endTime == time) {
                     allDoneWorks.add(inTimeWork);
+                    currentlyDoneWorks.add(inTimeWork);
                     inTimeWork.jobIsDone();
                     toDelete.add(inTimeWork);
                     for (Workers worker : workers) {
@@ -81,22 +87,62 @@ public class Main {
                     }
                 }
             }
-            possibleWorks.sort(new Comparator<Work>() {
-                @Override
-                public int compare(Work o1, Work o2) {
-                    if (o1.hours > o2.hours) return 1;
-                    else return (o1.hours < o2.hours) ? -1 : 0;
+            switch (Z) {
+                case 0: {
+                    possibleWorks.sort(new Comparator<Work>() {
+                        @Override
+                        public int compare(Work o1, Work o2) {
+                            if (o1.hours > o2.hours) return 1;
+                            else return (o1.hours < o2.hours) ? -1 : 0;
+                        }
+                    });
+                    break;
                 }
-            });
-            if (time == 21) {
-                possibleWorks.remove(work47);
-                possibleWorks.add(work47);
+                case 1: {
+                    possibleWorks.sort(new Comparator<Work>() {
+                        @Override
+                        public int compare(Work o1, Work o2) {
+                            if (o1.hours < o2.hours) return 1;
+                            else return (o1.hours > o2.hours) ? -1 : 0;
+                        }
+                    });
+                    break;
+                }
+                case 2: {
+                    possibleWorks.sort(new Comparator<Work>() {
+                        @Override
+                        public int compare(Work o1, Work o2) {
+                            if (o1.additionalHours > o2.additionalHours) return 1;
+                            else return (o1.additionalHours < o2.additionalHours) ? -1 : 0;
+                        }
+                    });
+                    break;
+                }
+                case 3: {
+                    possibleWorks.sort(new Comparator<Work>() {
+                        @Override
+                        public int compare(Work o1, Work o2) {
+                            if (o1.additionalHours < o2.additionalHours) return 1;
+                            else return (o1.additionalHours > o2.additionalHours) ? -1 : 0;
+                        }
+                    });
+                    break;
+                }
+
             }
-            System.out.print("time = " + time);
-            System.out.print("   possibleWorks:");
+
+            System.out.println("time = " + time);
+            System.out.print("possibleWorks:");
             possibleWorks.removeAll(inTimeWorks);
             for (Work posW : possibleWorks) {
-                System.out.print(posW.workName + ", ");
+                System.out.print(posW.workName + "-" + posW.hours +"h, ");
+            }
+            System.out.println();
+            for (Workers workers1 : workers) {
+                if (!workers1.isWorking)
+                    System.out.println("worker" + workers.indexOf(workers1) + " isn't working atm");
+                else
+                    System.out.println("worker" + workers.indexOf(workers1) + " ends at " + workers1.endTime);
             }
             for (Workers worker : workers) {
                 if (!worker.isWorking && possibleWorks.size() != 0) {
@@ -115,15 +161,18 @@ public class Main {
 
 
             inTimeWorks.removeAll(toDelete);
-            System.out.print("   Done works:");
-            for (Work doWo : allDoneWorks) {
+            System.out.print("Done works:");
+            for (Work doWo : currentlyDoneWorks) {
                 System.out.print(doWo.workName + ", ");
             }
-            System.out.print("  ATM works:");
+            System.out.println();
+            System.out.print("ATM works:");
             for (Work atmWorks : inTimeWorks) {
-                System.out.print(atmWorks.workName + ", ");
+                System.out.print(atmWorks.workName + " ends at time = " + atmWorks.endTime + ", ");
             }
             System.out.println();
+            System.out.println("--------------------------------------------------------------------------------------");
+            currentlyDoneWorks.clear();
             time ++;
         }
     }
