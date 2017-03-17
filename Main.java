63c1a7eef1f0f83170a3506f972f576de0ffe0f6
Andrei <bloodyavangard@mail.ru>
2017-03-17 12:47:14 +0300

    deleted redundant init

--- Main.java
+++ Main.java
@@ -19,7 +19,7 @@ public class Main {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        while(scanner.hasNext()) {
+        while (scanner.hasNext()) {
             String[] splittedString = scanner.nextLine().split(" ");
             List<String> splittedWorks = Arrays.asList(splittedString[3].split(","));
             List<Work> prevWorks = new ArrayList<>();
@@ -30,51 +30,9 @@ public class Main {
                     }
                 }
             }
-            allWorksList.add(new Work(splittedString[0], Integer.parseInt(splittedString[1]),Integer.parseInt(splittedString[2]), prevWorks));
-            //prevWorks.clear();
+            allWorksList.add(new Work(splittedString[0], Integer.parseInt(splittedString[1]), Integer.parseInt(splittedString[2]), prevWorks));
         }
         allWorksList.remove(fW);
-//        Work dW = new Work("dw", 0, 0, null);
-//        dW.jobIsDone();
-//        List<Work> dWList = new ArrayList<>();
-//        dWList.add(dW);
-//        Work work12 = new Work("w12", 2, 0, dWList);
-//        List<Work> doneWorks = new ArrayList<>();
-//        doneWorks.add(work12);
-//        Work work13 = new Work("w13", 3, 2, dWList);
-//        List<Work> doneWorks_1 = new ArrayList<>();
-//        doneWorks_1.add(work13);
-//        Work work24 = new Work("w24", 7, 0, doneWorks);
-//        Work work35 = new Work("w35", 6, 4, doneWorks_1);
-//        Work work34 = new Work("w34", 4, 2, doneWorks_1);
-//        Work work25 = new Work("w25", 4, 7, doneWorks);
-//        List<Work> doneWorks_2 = new ArrayList<>();
-//        doneWorks_2.add(work25);
-//        doneWorks_2.add(work35);
-//        Work work56 = new Work("w56", 6, 5, doneWorks_2);
-//        Work work57 = new Work("w57", 3, 6, doneWorks_2);
-//        Work work58 = new Work("w58", 4, 4, doneWorks_2);
-//        List<Work> doneWorks_3 = new ArrayList<>();
-//        doneWorks_3.add(work24);
-//        doneWorks_3.add(work34);
-//        Work work46 = new Work("w46", 9, 2, doneWorks_3);
-//        Work work47 = new Work("w47", 6, 3, doneWorks_3);
-//        Work work48 = new Work("w48", 8, 0, doneWorks_3);
-//        List<Work> doneWorks_4 = new ArrayList<>();
-//        doneWorks_4.add(work46);
-//        doneWorks_4.add(work56);
-//        Work work69 = new Work("w69", 5, 2, doneWorks_4);
-//        List<Work> doneWorks_5 = new ArrayList<>();
-//        doneWorks_5.add(work47);
-//        doneWorks_5.add(work57);
-//        Work work79 = new Work("w79", 7, 3, doneWorks_5);
-//        List<Work> doneWorks_6 = new ArrayList<>();
-//        doneWorks_6.add(work48);
-//        doneWorks_6.add(work58);
-//        Work work89 = new Work("w89", 8, 0, doneWorks_6);
-
-//        Work[] workArray = {work12, work13, work24, work25, work34, work35, work46, work47, work48, work56, work57, work58, work69, work79, work89};
-//        allWorksList = Arrays.asList(workArray);
 
         List<Workers> workers = new ArrayList<>();
         for (int i = 0; i < N; i++)
@@ -155,14 +113,13 @@ public class Main {
                     });
                     break;
                 }
-
             }
 
             System.out.println("time = " + time);
             System.out.print("possibleWorks:");
             possibleWorks.removeAll(inTimeWorks);
             for (Work posW : possibleWorks) {
-                System.out.print(posW.workName + "-" + posW.hours +"h, ");
+                System.out.print(posW.workName + "-" + posW.hours + "h, ");
             }
             System.out.println();
             for (Workers workers1 : workers) {
@@ -181,7 +138,7 @@ public class Main {
                             works.endTime = time + works.hours;
                         }
                     }
-                    possibleWorks.remove(possibleWorks.size()-1);
+                    possibleWorks.remove(possibleWorks.size() - 1);
                 }
             }
             possibleWorks.clear();
@@ -200,7 +157,7 @@ public class Main {
             System.out.println();
             System.out.println("--------------------------------------------------------------------------------------");
             currentlyDoneWorks.clear();
-            time ++;
+            time++;
         }
     }
 }
