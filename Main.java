557e38d338e433494ca616ae73be0856802b7333
Andrei <bloodyavangard@mail.ru>
2017-03-20 22:23:04 +0300

    diagram added. reserved time added. output to file

--- Main.java
+++ Main.java
@@ -3,10 +3,12 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
         final int N = 2;
         final int Z = 0;
         List<Work> allWorksList = new ArrayList<>();
@@ -42,7 +44,8 @@ public class Main {
         List<Work> allDoneWorks = new ArrayList<>();
         List<Work> inTimeWorks = new ArrayList<>();
         List<Work> currentlyDoneWorks = new ArrayList<>();
-
+        String fileName = "default_" + String.valueOf(Z) + "_Neq" + String.valueOf(N) + ".txt";
+        PrintWriter writer = new PrintWriter(fileName, "UTF-8");
         int time = 0;
         while (allDoneWorks.size() != allWorksList.size()) {
             List<Work> toDelete = new ArrayList<>();
@@ -81,6 +84,9 @@ public class Main {
                             else return (o1.hours < o2.hours) ? -1 : 0;
                         }
                     });
+                    if (time == 0) {
+                        writer.println("Amax" + ", N = " + N);
+                    }
                     break;
                 }
                 case 1: {
@@ -91,6 +97,9 @@ public class Main {
                             else return (o1.hours > o2.hours) ? -1 : 0;
                         }
                     });
+                    if (time == 0) {
+                        writer.println("Amin" + ", N = " + N);
+                    }
                     break;
                 }
                 case 2: {
@@ -101,6 +110,9 @@ public class Main {
                             else return (o1.additionalHours < o2.additionalHours) ? -1 : 0;
                         }
                     });
+                    if (time == 0) {
+                        writer.println("Rmax" + ", N = " + N);
+                    }
                     break;
                 }
                 case 3: {
@@ -111,27 +123,31 @@ public class Main {
                             else return (o1.additionalHours > o2.additionalHours) ? -1 : 0;
                         }
                     });
+                    if (time == 0) {
+                        writer.println("Rmin" + ", N = " + N);
+                    }
                     break;
                 }
             }
 
-            System.out.println("time = " + time);
-            System.out.print("possibleWorks:");
+            writer.println("time = " + time);
+            writer.println("possibleWorks:");
             possibleWorks.removeAll(inTimeWorks);
             for (Work posW : possibleWorks) {
-                System.out.print(posW.workName + "-" + posW.hours + "h, ");
+                writer.println("     " + posW.workName + ": time = " + posW.hours + "h, reserved time = " + posW.additionalHours + "h");
             }
-            System.out.println();
+            writer.println("workers:");
             for (Workers workers1 : workers) {
                 if (!workers1.isWorking)
-                    System.out.println("worker" + workers.indexOf(workers1) + " isn't working atm");
+                    writer.println("     worker" + workers.indexOf(workers1) + " isn't working at the moment");
                 else
-                    System.out.println("worker" + workers.indexOf(workers1) + " ends at time = " + workers1.endTime);
+                    writer.println("     worker" + workers.indexOf(workers1) + " ends at time = " + workers1.endTime + " doing " + workers1.currentWork);
             }
             for (Workers worker : workers) {
                 if (!worker.isWorking && possibleWorks.size() != 0) {
                     worker.isWorking = true;
                     worker.endTime = time + possibleWorks.get((possibleWorks.size() - 1)).hours;
+                    worker.currentWork = possibleWorks.get((possibleWorks.size() - 1)).workName;
                     inTimeWorks.add(possibleWorks.get((possibleWorks.size() - 1)));
                     for (Work works : allWorksList) {
                         if (possibleWorks.get((possibleWorks.size() - 1)).workName.equals(works.workName)) {
@@ -145,19 +161,50 @@ public class Main {
 
 
             inTimeWorks.removeAll(toDelete);
-            System.out.print("Done works:");
+            writer.print("Done works:");
             for (Work doWo : currentlyDoneWorks) {
-                System.out.print(doWo.workName + ", ");
+                writer.print(doWo.workName + ", ");
             }
-            System.out.println();
-            System.out.print("ATM works:");
+            writer.println();
+            writer.println("At the moment works:");
             for (Work atmWorks : inTimeWorks) {
-                System.out.print(atmWorks.workName + " ends at time = " + atmWorks.endTime + ", ");
+                writer.println("     " + atmWorks.workName + " ends at time = " + atmWorks.endTime);
             }
-            System.out.println();
-            System.out.println("--------------------------------------------------------------------------------------");
+            for (Workers w : workers) {
+                if (w.isWorking) {
+                    w.hold.add("+");
+                } else {
+                    w.hold.add("-");
+                }
+            }
+            writer.println();
+            writer.println("--------------------------------------------------------------------------------------");
             currentlyDoneWorks.clear();
             time++;
         }
+//        for (Workers wo : workers){
+//            if (wo.endTime == time - 1) {
+//                wo.hold.remove(wo.hold.size() - 1);
+//                wo.hold.add("+");
+//            }
+//        }
+        int i = 0;
+        for (Workers w : workers) {
+            writer.print("worker" + i + " :");
+            int count = 0;
+            for (String h : w.hold) {
+                writer.print(h + " ");
+                if(count > 9)
+                    writer.print(" ");
+                count++;
+            }
+            writer.println();
+            i++;
+        }
+        writer.print("time    :");
+        for (int j = 0; j < time; j++) {
+            writer.print(j + " ");
+        }
+        writer.close();
     }
 }
--- Workers.java
+++ Workers.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 809279 on 16.03.2017.
  */
 public class Workers {
     public boolean isWorking = false;
     public int endTime;
+    public List<String> hold = new ArrayList<>();
+    public String currentWork = "";
     public Workers() {
         this.endTime = 0;
     }
